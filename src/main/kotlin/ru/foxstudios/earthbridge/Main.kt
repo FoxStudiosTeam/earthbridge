3b30173a6296f190fe0e900a3798fa7e397b7f7d
AgniaEndie <Endienasg3@gmail.com>
2024-03-07 19:59:55 +0500

    test UDP protocol t-0.1

--- src/main/kotlin/ru/foxstudios/earthbridge/Main.kt
+++ src/main/kotlin/ru/foxstudios/earthbridge/Main.kt
@@ -1,22 +1,34 @@
 package ru.foxstudios.earthbridge
 
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+import io.netty.buffer.Unpooled
+import io.netty.channel.ChannelOption
+import io.netty.channel.socket.DatagramPacket
 import kotlinx.coroutines.runBlocking
-import org.slf4j.LoggerFactory
-import reactor.netty.Connection
+import reactor.core.publisher.Flux
 import reactor.netty.udp.UdpServer
-import java.time.Duration
+import java.nio.charset.StandardCharsets
 
 
 fun main(args: Array<String>) {
-    val server: Connection = UdpServer.create().port(25577).host("127.0.0.1").bindNow(Duration.ofSeconds(10))
     runBlocking {
-        launch {
-            println("starting2")
-        }
-        println("starting1")
+        println("da")
     }
-    server.onDispose().block()
+    val server = UdpServer.create().port(25577).host("127.0.0.1").option(ChannelOption.SO_BROADCAST, true)
+        .handle { inbound, outbound ->
+            val inFlux: Flux<DatagramPacket> = inbound.receiveObject()
+                .handle { incoming, sink ->
+                    if (incoming is DatagramPacket) {
+                        val packet = incoming
+                        val content = packet.content().toString(StandardCharsets.UTF_8)
+                        println(content)
+
+                        val byteBuf = Unpooled.copiedBuffer("ok", StandardCharsets.UTF_8)
+                        val response = DatagramPacket(byteBuf, packet.sender())
+                        sink.next(response)
+                    }
+                }
+            return@handle outbound.sendObject(inFlux)
+        }
+    server.bindNow().onDispose().block()
 }
 
