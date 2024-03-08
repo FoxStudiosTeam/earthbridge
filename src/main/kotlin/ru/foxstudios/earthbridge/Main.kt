7ad2fe265f0e635f5c2ec7b0d53f870b57cf2ef3
AgniaEndie <Endienasg3@gmail.com>
2024-03-08 18:08:21 +0500

    add bufferedWriter

--- src/main/kotlin/ru/foxstudios/earthbridge/Main.kt
+++ src/main/kotlin/ru/foxstudios/earthbridge/Main.kt
@@ -1,11 +1,15 @@
 package ru.foxstudios.earthbridge
 
+import com.rabbitmq.client.impl.nio.ByteBufferOutputStream
 import io.netty.buffer.Unpooled
 import io.netty.channel.ChannelOption
 import io.netty.channel.socket.DatagramPacket
 import kotlinx.coroutines.runBlocking
 import reactor.core.publisher.Flux
+import reactor.core.scheduler.Schedulers
 import reactor.netty.udp.UdpServer
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
 import java.nio.charset.StandardCharsets
 
 
@@ -16,13 +20,16 @@ fun main(args: Array<String>) {
     val server = UdpServer.create().port(25577).host("127.0.0.1").wiretap(true).option(ChannelOption.SO_BROADCAST, true)
         .handle { inbound, outbound ->
             val inFlux: Flux<DatagramPacket> = inbound.receiveObject()
+                .publishOn(Schedulers.boundedElastic())
                 .handle { incoming, sink ->
                     if (incoming is DatagramPacket) {
                         val packet = incoming
                         val content = packet.content().toString(StandardCharsets.UTF_8)
-                        println(content.toByteArray().size)
-                        println(content)
-
+                        val writer = BufferedWriter(OutputStreamWriter(System.out))
+                        writer.write(content.toByteArray().size)
+                        writer.flush()
+                        writer.write(content)
+                        writer.flush()
                         val byteBuf = Unpooled.copiedBuffer("ok", StandardCharsets.UTF_8)
                         val response = DatagramPacket(byteBuf, packet.sender())
                         sink.next(response)
